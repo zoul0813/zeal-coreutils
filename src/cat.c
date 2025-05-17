c0ed6ac3995a9a4a874760bf4e1d0cd10b7beb11
David Higgins <zoul0813@me.com>
2025-05-17 10:37:56 -0400

    * added cat * added echo

--- src/cat.c
+++ src/cat.c
@@ -1,5 +1,42 @@
 #include <zos_errors.h>
+#include <zos_vfs.h>
+#include "common.h"
 
-int main(void) {
-    return ERR_NOT_IMPLEMENTED;
+#define BUFFER_SIZE 1024
+
+zos_err_t err;
+zos_dev_t dev;
+uint16_t size;
+uint16_t addr;
+char buffer[BUFFER_SIZE];
+
+void put_hex(char c) {
+
+}
+
+int main(int argc, char** argv) {
+    if(argc == 0) {
+        put_s("usage: echo <string>\n");
+        return ERR_INVALID_PARAMETER;
+    }
+    if(argc == 1) {
+        dev = open(argv[0], O_RDONLY);
+        if(dev < 0) {
+            put_s(argv[0]); put_s(" not found\n");
+            return -dev;
+        }
+
+        size = BUFFER_SIZE;
+        do {
+            err = read(dev, buffer, &size);
+            if(size > 0) err = write(DEV_STDOUT, buffer, &size);
+        } while(err == ERR_SUCCESS && size > 0);
+        err = close(dev);
+
+        if(err != ERR_SUCCESS) {
+            put_s(argv[0]); put_s(" error reading\n");
+            return err;
+        }
+        return err;
+    }
 }
--- src/echo.c
+++ src/echo.c
@@ -1,5 +1,16 @@
 #include <zos_errors.h>
+#include <zos_vfs.h>
+#include "common.h"
 
-int main(void) {
-    return ERR_NOT_IMPLEMENTED;
+zos_err_t err;
+
+int main(int argc, char** argv) {
+    if(argc == 1) {
+        put_s(argv[0]);
+        put_c(CH_NEWLINE);
+        return ERR_SUCCESS;
+    }
+
+    put_s("usage: echo <string>\n");
+    return ERR_INVALID_PARAMETER;
 }
