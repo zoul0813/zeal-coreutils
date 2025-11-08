ebe5d7e7a76c294c49b7c5ffddd16fbba0efb10f
David Higgins <zoul0813@me.com>
2025-11-08 15:33:22 -0500

    add argument parsing to mkdir to support `-p` and `-v` flags

--- src/mkdir.c
+++ src/mkdir.c
@@ -4,24 +4,53 @@
 
 zos_err_t err;
 
-int main(int argc, char** argv)
-{
-    if (argc == 0) {
-        put_s("usage: mkdir <dst>\n");
+typedef enum {
+    OPTION_NONE = 0,
+    OPTION_PREFIX = 1,
+    OPTION_VERBOSE = 2,
+} option_e;
+
+void usage(void) {
+    put_s("usage: mkdir [-pv] dir_name\n");
+}
+
+int main(int argc, char** argv) {
+    if(argc == 0) {
+        usage();
         return ERR_INVALID_PARAMETER;
     }
 
-    char* src = argv[0];
-    char* dst = NULL;
+    char* params = argv[0];
+    option_e options = OPTION_NONE;
 
-    while (*src) {
-        if (*src == CH_SPACE) {
-            *src = 0;
-            dst  = src + 1;
+    if(*params == '-') {
+        params++;
+        while(params) {
+            switch(*params) {
+                case 'p': {
+                    options |= OPTION_PREFIX;
+                } break;
+                case 'v': {
+                    options |= OPTION_VERBOSE;
+                } break;
+                case 'h': {
+                    usage();
+                    return ERR_SUCCESS;
+                } break;
+                case '\0':
+                case ' ':
+                    goto parsed;
+                default: {
+                    put_s("Invalid option: ");
+                    put_s(params);
+                    put_c(CH_NEWLINE);
+                    return ERR_INVALID_PARAMETER;
+                }
+            }
         }
-        src++;
     }
-    src = argv[0];
-
+parsed:
+    while(*params == CH_SPACE) params++;
+    char *src = params;
     return mkdir(src);
 }
